75fe7eee6a8d88c768ccdfb396e2c122b04a2c76
Deloris Thompson <deloristhompson28@gmail.com>
2020-02-05 12:58:59 -0500

    Various Dev and UI updates

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
+gem 'bootstrap', '~> 4.4.1'
+gem 'sprockets-rails', '~> 3.0', '>= 3.0.4'
+gem 'popper_js', '~> 1.16'
 group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
@@ -14,7 +17,7 @@ end
 
 group :development, :test do
   gem 'capybara'
-  gem 'factory_girl_rails'
+  gem 'factory_bot_rails'
   gem 'launchy', require: false
   gem 'pry-rails'
   gem 'rspec-rails', '~> 3.5'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200130154516) do
+ActiveRecord::Schema.define(version: 20200205160316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20200130154516) do
     t.string "image"
     t.string "description"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
